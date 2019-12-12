8b2c68dd4683df23cfa4e6a9eb5198ce7737dadb
Matt Daniels <matt@techtoniq.com>
2019-12-12 09:40:07 +0000

    Dummy code to enable build pipeline

---
+++ DFC.Personalisation.CommonUI.UnitTests/DummyToBeRemovedTests.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace DFC.Personalisation.CommonUI.UnitTests
+{
+    [TestFixture]
+    public class DummyToBeRemovedTests
+    {
+        [Test]
+        public void tdd()
+        {
+            var sut = new DummyToBeRemoved();
+
+            sut.RemoveMe();
+
+            sut.Should().NotBeNull();
+        }
+    }
+}
---
+++ DFC.Personalisation.CommonUI/DummyToBeRemoved.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DFC.Personalisation.CommonUI
+{
+    public class DummyToBeRemoved
+    {
+        public void RemoveMe()
+        {
+
+        }
+    }
+}
