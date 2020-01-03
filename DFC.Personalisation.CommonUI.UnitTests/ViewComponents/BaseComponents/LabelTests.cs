3d380c059b02271982762b26f071c2556b743167
Stephen Yates <SYatesSoftware@gmail.com>
2020-01-03 13:56:25 +0000

    radio buttons fix

--- DFC.Personalisation.CommonUI.UnitTests/ViewComponents/BaseComponents/LabelTests.cs
+++ DFC.Personalisation.CommonUI.UnitTests/ViewComponents/BaseComponents/LabelTests.cs
@@ -4,6 +4,7 @@ using NSubstitute;
 using NUnit.Framework;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using DFC.Personalisation.CommonUI.ViewComponents.Components.RadioButton;
 
 namespace DFC.Personalisation.CommonUI.UnitTests.ViewComponents.BaseComponents
 {
@@ -11,7 +12,7 @@ namespace DFC.Personalisation.CommonUI.UnitTests.ViewComponents.BaseComponents
     {
         [TestCase(nameof(LabelModel.Text), "The Text Field has Been Set")]
         [TestCase(nameof(LabelModel.For), "The Text Field has Been Set")]
-        public void WhenErrorInvoked_TheViewModelIsUpdated(string key, string value)
+        public void WhenLabelInvoked_TheViewModelIsUpdated(string key, string value)
         {
             var values = new Dictionary<string, string>() { { key, value } };
 
@@ -25,13 +26,37 @@ namespace DFC.Personalisation.CommonUI.UnitTests.ViewComponents.BaseComponents
             Assert.AreEqual(value, ViewComponentTestHelper.GetPropertyValue(resultModel, key));
         }
 
+        [TestCase(nameof(LabelModel.AdditionalClass), "govuk-radios__label")]
+        public void WhenRadioLabelInvoked_TheViewModelIsUpdated(string key, string value)
+        {
+            var values = new Dictionary<string, string>() { { key, value } };
+
+            var component = new RadioLabel();
+            component.ViewComponentContext = ViewComponentTestHelper.GeViewComponentContext();
+
+            ViewViewComponentResult result = component.Invoke(values) as ViewViewComponentResult;
+            LabelModel resultModel = (LabelModel)result.ViewData.Model;
+
+            //Assert
+            Assert.AreEqual(value, ViewComponentTestHelper.GetPropertyValue(resultModel, key));
+        }
+
         [Test]
-        public async Task WhenErrorTagHelperCalled_ThenCorrectClassCalled()
+        public async Task WhenLabelTagHelperCalled_ThenCorrectClassCalled()
         {
             var tagHelper = Substitute.For<IMockViewComponentHelper>();
 
             var componentTag = new LabelTagHelper(tagHelper);
             await ViewComponentTestHelper.CallTagHelper("Label", tagHelper, componentTag);
+        }
+
+        [Test]
+        public async Task WhenRadioLabelTagHelperCalled_ThenCorrectClassCalled()
+        {
+            var tagHelper = Substitute.For<IMockViewComponentHelper>();
+
+            var componentTag = new RadioLabelTagHelper(tagHelper);
+            await ViewComponentTestHelper.CallTagHelper("RadioLabel", tagHelper, componentTag);
         }
     }
 }
