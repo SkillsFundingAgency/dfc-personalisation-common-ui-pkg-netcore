66242398caa4b9b3b71563f1ffc1deeed394155d
Stephen Yates <SYatesSoftware@gmail.com>
2020-01-09 14:24:11 +0000

    added component w3 validation tests

---
+++ DFC.Personalisation.CommonUI.IntegrationTests/Models/MessageDetails.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+
+namespace DFC.Personalisation.CommonUI.IntegrationTests.Models
+{
+    public class MessageDetails
+    {
+        public string Type { get; set; }
+        public int LastLine { get; set; }
+        public int LastColumn { get; set; }
+        public int FirstColumn { get; set; }
+        public SubType SubType { get; set; }
+        public string Message { get; set; }
+        public string Extract { get; set; }
+    }
+
+    public enum SubType
+    {
+        Undifined,
+        Warning,
+        Fatal,
+        [JsonProperty("non-document-error")]
+        NonDocumentError
+    }
+}
