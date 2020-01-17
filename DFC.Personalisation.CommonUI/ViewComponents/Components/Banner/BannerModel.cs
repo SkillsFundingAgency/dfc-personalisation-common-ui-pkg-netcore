b0ec4a56f7d15ea6a50ed574ce0bd78bb62bf559
Liam Wright <mail@liam-wright.co.uk>
2020-01-17 15:30:49 +0000

    Added PrimaryHeroBanner Component

---
+++ DFC.Personalisation.CommonUI/ViewComponents/Components/Banner/BannerModel.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace DFC.Personalisation.CommonUI.ViewComponents.Components.Banner
+{
+    public class BannerModel : ViewModelBase, IBanner
+    {
+        public string Id { get; set; }
+        public string HeaderText { get; set; }
+        public string SubheaderText { get; set; }
+    }
+}
---
+++ DFC.Personalisation.CommonUI/ViewComponents/Components/Banner/IBanner.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace DFC.Personalisation.CommonUI.ViewComponents.Components.Banner
+{
+    public interface IBanner
+    {
+        string Id { get; set; }
+        [HtmlAttributeName("headerText")]
+        string HeaderText { get; set; }
+        [HtmlAttributeName("subheaderText")]
+        string SubheaderText { get; set; }
+    }
+}
