d648381ae1b3938e4d928807be4a205a95f33e9d
Liam Wright <mail@liam-wright.co.uk>
2020-01-20 14:48:10 +0000

    Removed ID field as it's inherited by IHtmlBase

--- DFC.Personalisation.CommonUI/ViewComponents/Components/Button/IButtonAttributes.cs
+++ DFC.Personalisation.CommonUI/ViewComponents/Components/Button/IButtonAttributes.cs
@@ -4,7 +4,6 @@ namespace DFC.Personalisation.CommonUI.ViewComponents.Components.Button
 {
     public interface IButtonAttributes : IAdditional, IHtmlBase
     {
-        string Id { get; set; }
         string Text { get; set; }
         bool Disabled { get; set; }
     }
